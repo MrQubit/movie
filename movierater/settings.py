de57fd999d06d6c8e6836375b93c1115a4dca558
Zigo <peter.zigo@siemens-healthineers.com>
2020-04-16 11:25:51 +0200

    Added cors

--- movierater/settings.py
+++ movierater/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
+    'corsheaders',
     'api'
 ]
 
@@ -50,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'movierater.urls'
@@ -92,6 +94,10 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated'
     }
 }
+
+CORS_ORIGIN_WHITELIST = [
+    "http://localhost:4200"
+]
 
 
 # Password validation
